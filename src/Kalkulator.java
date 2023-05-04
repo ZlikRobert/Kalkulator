d4a94d83fc44c5e7df273d76721f6fc35cc389d5
Robert <zlikrobert@gmail.com>
2023-05-04 20:38:46 +0200

    Basic Calculator

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Kalkulator {
+// A class that provides basic mathematical operations such as addition, subtraction, multiplication, and division
+public class Kalkulator {
+    // A static method that adds two numbers and returns the result
+    public static double dodaj(double a, double b) {
+        return a + b;
+    }
+    // A static method that subtracts one number from another and returns the result
+    public static double odejmij(double a, double b) {
+        return a - b;
+    }
+    // A static method that multiplies two numbers and returns the result
+    public static double pomnoz(double a, double b) {
+        return a * b;
+    }
+    // A static method that divides one number by another and returns the result. If the second number is zero, an exception is thrown.
+    public static double podziel(double a, double b) throws IllegalArgumentException {
+        if (b == 0) {
+            throw new IllegalArgumentException("Nie można dzielić przez zero!");
+        }
+        return a / b;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,62 @@
+// Import the Scanner object for user input
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        // Create a Scanner object for user input
+        Scanner input = new Scanner(System.in);
+
+        // Display a welcome message and menu options
+        System.out.println("Witaj w kalkulatorze!");
+        System.out.println("Wybierz rodzaj operacji:");
+        System.out.println("1. Dodawanie");
+        System.out.println("2. Odejmowanie");
+        System.out.println("3. Mnożenie");
+        System.out.println("4. Dzielenie");
+        System.out.print("Twój wybór: ");
+
+        // Read the user's choice from input
+        int wybor = input.nextInt();
+
+        // Prompt the user to enter the two numbers
+        System.out.print("Podaj pierwszą liczbę: ");
+        double liczba1 = input.nextDouble();
+
+        System.out.print("Podaj drugą liczbę: ");
+        double liczba2 = input.nextDouble();
+
+        double wynik;
+
+        // Use a switch statement to perform the selected operation
+        switch (wybor) {
+            case 1:
+                wynik = Kalkulator.dodaj(liczba1, liczba2);
+                System.out.println("Wynik: " + wynik);
+                break;
+            case 2:
+                wynik = Kalkulator.odejmij(liczba1, liczba2);
+                System.out.println("Wynik: " + wynik);
+                break;
+            case 3:
+                wynik = Kalkulator.pomnoz(liczba1, liczba2);
+                System.out.println("Wynik: " + wynik);
+                break;
+            case 4:
+                // Use try-catch to handle division by zero
+                try {
+                    wynik = Kalkulator.podziel(liczba1, liczba2);
+                    System.out.println("Wynik: " + wynik);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+                System.out.println("Niepoprawny wybór!");
+                break;
+        }
+
+        // Close the Scanner object
+        input.close();
     }
+
 }
